bc52d632806979952ae89586dbffe18407802b0f
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-08-13 14:22:38 +0530

    file handling

---
+++ 13-08-2006/openfile.py
@@ -0,0 +1,4 @@
+f = open("demo.txt", "w")
+f.write("Hello,Python this is mee.")
+f.close()
+print("This file is created by me....")
---
+++ 13-08-2006/withfile.py
@@ -0,0 +1,4 @@
+with open("demo2.txt", "w") as f:
+    f.write("Hello Sakshi")
+
+print("file is created")
