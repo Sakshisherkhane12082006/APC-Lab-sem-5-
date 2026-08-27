dc5596260ca23c7f278b085437ef9e40cd571a05
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-08-27 22:11:26 +0530

    class and function

---
+++ 27-08-2026/class_fun_var_constructor.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self):
+        self.name = "Sakshi"
+        self.age = 20
+
+    def display(self):
+        print(self.name)
+        print(self.age)
+        
+s = Student()
+s.display()
