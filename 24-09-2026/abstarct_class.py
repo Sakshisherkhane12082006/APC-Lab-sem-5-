416835528220a56b37126a142225cac6eb20d54d
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-09-24 14:08:09 +0530

    abstract class

---
+++ 24-09-2026/abstarct_class.py
@@ -0,0 +1,11 @@
+from abc import ABC
+
+class Student(ABC):
+    pass
+
+class CollegeStudent(Student):
+    def study(self):
+        print("Student is studying")
+
+s = CollegeStudent()
+s.study()
