643c82741747ee02d59e3cf1534b5c66e4feeb11
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-09-24 15:09:12 +0530

    exception handling

--- 24-09-2026/user_defined_exception.py
+++ 24-09-2026/user_defined_exception.py
@@ -1,9 +1,10 @@
 class AgeError(Exception):
     pass
 try:
-    age=int(input("Enter your age: "))
-    if age<18:
-        raise AgeError
-    print("You are eligible")
-except AgeError:
-    print("Age must be 18 or above")
+    age = int(input("Enter your age: "))
+    if age < 18:
+        print("AgeError: Age must be 18 or above")
+    else:
+        print("You are eligible")
+except ValueError:
+    print("Please enter a valid age")
